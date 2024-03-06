ed6596691ad8b3333e6177d3741e6fbd94812460
YoussefMu23 <dembouz719@gmail.com>
2024-03-06 14:01:16 +0100

    update view_content

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 class ShoppingList:
-  #I konstruktorn skapa en lista för varorna
-  def __init__(self):
-    self.items = []
+    # I konstruktorn skapa en lista för varorna
+    def __init__(self):
+        self.items = []
 
-  #En metod för att lägga till.
-  # Kolla så att värdet är en text (string)
-  def add_item(self, item):
-    if not isinstance(item, str):
-      print("Item is not a string")
-      return False
+    # En metod för att lägga till.
+    # Kolla så att värdet är en text (string)
+    def add_item(self, item):
+        if not isinstance(item, str):
+          print("Item is not a string")
+          return False
 
-    self.items.append(item)
+        self.items.append(item)
 
-  #En metod för att se listans innehåll
+  # En metod för att se listans innehåll
   # Om listan är tom, skriv ut "list is empty"
-  def view_content(self):
-    if not (self.items):
-      print("List is empty")
-      return False
+    def view_content(self):
+      if not (self.items):
+        print("List is empty")
+        return False
 
-  #Ta bort det första item:et som stämmer överens med parametern "item"
+      for i, item in enumerate(self.items, start=1):
+        print(i, item, sep=" - ")
+
+  # Ta bort det första item:et som stämmer överens med parametern "item"
   # Om listan inte innehåller "item", skriv ut "item coult not be found"
-  def delete_item(self, item):
-    if not (self, item):
-      print(f"{item} could not be found")
-      return False
+    def delete_item(self, item):
+      if not (self, item):
+        print(f"{item} could not be found")
+        return False
 
-    self.items.remove(item)
+      self.items.remove(item)
 
 
 ## ÖVNING: Skapa ett program där ni implementerar ett konsol gränssnitt för att lägga till varor, se innehåll, och ta bort en vara
 if __name__ == '__main__':
-  shoppingList = ShoppingList()
+    shoppingList = ShoppingList()
 
-  shoppingList.add_item("Orange")
-  shoppingList.add_item("Lemon")
+    shoppingList.add_item("Orange")
+    shoppingList.add_item("Lemon")
 
-  shoppingList.view_content()
+    shoppingList.view_content()
